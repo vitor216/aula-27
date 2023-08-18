2b731340e2124dde8b949369a44d0d714d2da865
vitor <eletrotecvitor@gmail.com>
2023-08-17 22:10:32 -0300

    acerto dos componentes

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,19 +1,19 @@
 <script setup>
-    const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarfiltro','editatarefatemp'])
+    const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarFiltro','editaTarefaTemp'])
 </script>
 
 <template>
     <form @submit.prevent="props.cadastraTarefa">
         <div class="row">
             <div class="col">
-                <input :value="props.tarefaTemp" @change="props.editatarefatemp" required
+                <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required
                     type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
             </div>
             <div class="col-md-2">
                 <button type="submit" class="btn btn-primary">Cadastrar</button>
             </div>
             <div class="col-md-2">
-                <select @change="props.trocarfiltro" class="form-control">
+                <select @change="props.trocarFiltro" class="form-control">
                     <option value="todas">Todas Tarefas</option>
                     <option value="Pendentes">Pendentes</option>
                     <option value="Finalizadas">Finalizadas</option>
